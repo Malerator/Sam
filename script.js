bebcff71f56a170a9e66a3787031068a64471646
malerator <belik7337@gmail.com>
2023-11-13 23:57:35 +0300

    feat

---
+++ script.js
@@ -0,0 +1,189 @@
+const popUp = document.createElement("div");
+popUp.className = "modal";
+
+const form = document.querySelector("form");
+form.className = "form";
+
+const btn = document.querySelector(".btn");
+
+const closeBtn = document.createElement("button");
+closeBtn.className = "closeModalBtn";
+closeBtn.type = "button";
+
+const img = document.createElement("img");
+img.src = "./images/close.svg";
+img.width = "20";
+img.height = "20";
+
+const alertOk = document.querySelector(".tab2");
+
+const sendBtn = document.querySelector(".sendBtn");
+
+const TOKEN = "5856059976:AAHi68Tu9T8jSghs6j6tlfVk1dZWWPw-PGc";
+
+const CHAT_ID = "-1001695833016";
+
+const URL = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
+
+closeBtn.append(img);
+form.append(closeBtn);
+popUp.append(form);
+document.body.append(popUp);
+
+btn.addEventListener("click", openModal);
+closeBtn.addEventListener("click", closeModal);
+popUp.addEventListener("click", closeModal);
+form.addEventListener("click", (event) => event.stopPropagation());
+
+function openModal() {
+  popUp.style.display = "flex";
+}
+
+function closeModal() {
+  popUp.style.display = "none";
+}
+
+form.addEventListener("submit", function (el) {
+  el.preventDefault();
+
+  let checkState = document.querySelector('input[name="name"]:checked').value;
+  let checkState1 = document.querySelector('input[name="name1"]:checked').value;
+  let checkState2 = document.querySelector('input[name="name2"]:checked').value;
+  let checkState3 = document.querySelector('input[name="name3"]:checked').value;
+
+  let message = `<b>ЗАЯВКА С САЙТА</b>\n\n`;
+  message += `<b>Cпособ связи: </b>${checkState}\n`;
+  message += `<b>Кому: </b>${checkState1}\n`;
+  message += `<b>Состоит ли в реестре НРС: </b>${checkState2}\n`;
+  message += `<b>Физ. или Юр.лицо: </b>${checkState3}\n`;
+  message += `<b>Телефон:  </b>${this.tel.value}`;
+  axios
+    .post(URL, {
+      chat_id: CHAT_ID,
+      parse_mode: "html",
+      text: message,
+    })
+    .then((res) => {
+      this.tel.value = "";
+      let radio = document.getElementsByName("name");
+      for (let i = 0; i < radio.length; i++) radio[i].checked = false;
+      let radio1 = document.getElementsByName("name1");
+      for (let i = 0; i < radio1.length; i++) radio1[i].checked = false;
+      let radio2 = document.getElementsByName("name2");
+      for (let i = 0; i < radio2.length; i++) radio2[i].checked = false;
+      let radio3 = document.getElementsByName("name3");
+      for (let i = 0; i < radio3.length; i++) radio3[i].checked = false;
+      closeModal();
+    })
+    .catch((err) => {
+      alert(
+        "Что-то пошло не так(((\nПерезагрузите страницу и попробуйте снова"
+      );
+    })
+    .finally(() => {
+      console.log("ok");
+    });
+});
+
+let currentTab = 0;
+
+showTab(currentTab);
+
+function showTab(n) {
+  const x = document.getElementsByClassName("tab");
+  x[n].style.display = "block";
+  if (n == 0) {
+    document.getElementById("prevBtn").style.display = "none";
+    document.getElementById("nextBtn").style.display = "inline";
+  } else {
+    sendBtn.style.display = "none";
+    document.getElementById("prevBtn").style.display = "inline";
+    document.getElementById("nextBtn").style.display = "inline";
+  }
+  if (n == x.length - 1) {
+    document.getElementById("nextBtn").style.display = "none";
+    sendBtn.style.display = "inline-block";
+  } else {
+    document.getElementById("nextBtn").innerHTML = "ВПЕРЕД";
+  }
+  fixStepIndicator(n);
+}
+
+function nextPrev(n) {
+  const x = document.getElementsByClassName("tab");
+  if (n == 1 && !validateForm()) return false;
+  x[currentTab].style.display = "none";
+  currentTab = currentTab + n;
+  showTab(currentTab);
+}
+
+function validateForm() {
+  let x,
+    y,
+    i,
+    valid = true;
+  x = document.getElementsByClassName("tab");
+  y = x[currentTab].getElementsByTagName("input");
+  for (i = 0; i < y.length; i++) {
+    if (y[i].value === "") {
+      y[i].className += " invalid";
+      valid = false;
+    }
+  }
+  if (valid) {
+    document.getElementsByClassName("step")[currentTab].className += " finish";
+  }
+  return valid;
+}
+
+function fixStepIndicator(num) {
+  let i,
+    bolProgres = document.getElementsByClassName("step");
+  for (i = 0; i < bolProgres.length; ++i) {
+    bolProgres[i].className = bolProgres[i].className.replace(" active", "");
+  }
+  bolProgres[num].className += " active";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  let input = document.querySelector(".maskphone");
+  input.addEventListener("input", mask);
+  input.addEventListener("focus", mask);
+  input.addEventListener("blur", mask);
+
+  function mask(event) {
+    let blank = "+_ (___) ___-__-__";
+    let i = 0;
+    let val = this.value.replace(/\D/g, "").replace(/^8/, "7");
+
+    this.value = blank.replace(/./g, function (char) {
+      if (/[_\d]/.test(char) && i < val.length) return val.charAt(i++);
+
+      return i >= val.length ? "" : char;
+    });
+
+    if (event.type == "blur") {
+      if (this.value.length == 2) this.value = "";
+    } else {
+      setCursorPosition(this, this.value.length);
+    }
+  }
+
+  function setCursorPosition(elem, pos) {
+    elem.focus();
+
+    if (elem.setSelectionRange) {
+      elem.setSelectionRange(pos, pos);
+      return;
+    }
+
+    if (elem.createTextRange) {
+      let range = elem.createTextRange();
+      range.collapse(true);
+      range.moveEnd("character", pos);
+      range.moveStart("character", pos);
+      range.select();
+      return;
+    }
+  }
+});
